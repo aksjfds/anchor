48cbafb995cd8c54bcbf3ca5f2a4f96025d3390f
aksjfds <2502371043@qq.com>
2025-09-03 22:21:52 +0800

    4.2

--- tests/entrypoint.ts
+++ tests/entrypoint.ts
@@ -2,6 +2,10 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import type { Entrypoint } from "../target/types/entrypoint";
 
+anchor.setProvider(anchor.AnchorProvider.env());
+const program = anchor.workspace.entrypoint as Program<Entrypoint>;
+const connection = program.provider.connection;
+
 describe("entrypoint", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -9,8 +13,17 @@ describe("entrypoint", () => {
   const program = anchor.workspace.entrypoint as Program<Entrypoint>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    printLogs(tx);
   });
 });
+
+
+// printLogs
+const printLogs = (signature: string) => {
+  connection.getParsedTransaction(signature, "confirmed").then(res => {
+    const logMessages = res.meta.logMessages
+      .filter(message => message.includes("log"));
+    console.log(logMessages);
+  });
+}
